e29ed1b27be01ef476e8238b75a2990dbf217b36
reemabhalani <reema.bhalani@gmail.com>
2023-06-14 18:19:28 -0700

    Update package.swift file

--- Package.swift
+++ Package.swift
@@ -8,21 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SPMDemoPackage",
-            targets: ["SPMDemoPackage"]),
+            name: "SPMDemoFramework",
+            targets: ["SPMDemoFramework"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SPMDemoPackage",
-            dependencies: []),
-        .testTarget(
-            name: "SPMDemoPackageTests",
-            dependencies: ["SPMDemoPackage"]),
+        .binaryTarget(
+            name: "SPMDemoFramework",
+            url: "https://github.com/reemabhalani/SPMDemoFramework/blob/main/SPMDemoFramework.xcframework.zip",
+            checksum: "bb1c996bd282ccd37d13478a5971a2b1ee54d1f7820b88ec48500e07a17df150"
+        )
     ]
 )
