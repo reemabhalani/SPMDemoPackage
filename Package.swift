b1936838859ff8a6f501dce30b3e87fc4fac31e3
reemabhalani <reema.bhalani@gmail.com>
2023-06-15 12:03:27 -0700

    Update package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SPMDemoPackage",
+    name: "SPMDemoFramework",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SPMDemoFramework",
-            url: "https://github.com/reemabhalani/SPMDemoFramework/blob/main/SPMDemoFramework.xcframework.zip",
+            url: "https://github.com/reemabhalani/SPMDemoPackage/releases/download/1.0.1/SPMDemoFramework.xcframework.zip",
             checksum: "bb1c996bd282ccd37d13478a5971a2b1ee54d1f7820b88ec48500e07a17df150"
         )
     ]
