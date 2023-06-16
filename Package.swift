6b80a59968d07d18f608ad88eab684d329bce8f4
reemabhalani <reema.bhalani@gmail.com>
2023-06-16 10:31:35 -0700

    Update release and wrong checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SPMDemoFramework",
-            url: "https://github.com/reemabhalani/SPMDemoPackage/releases/download/1.0.5/SPMDemoFramework-1.0.5.xcframework.zip",
+            url: "https://github.com/reemabhalani/SPMDemoPackage/releases/download/1.0.6/SPMDemoFramework-1.0.5.xcframework.zip",
             checksum: "1c0288d921d66f31afbec2f0196e898d9ad1d56ac81e8d6f3c5a99c98a49a4e3"
         )
     ]
