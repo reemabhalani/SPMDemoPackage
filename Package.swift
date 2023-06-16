983596a78a3993cdd5a0e7dcd7b5c139ecbb26b7
reemabhalani <reema.bhalani@gmail.com>
2023-06-16 10:13:23 -0700

    Add wrong checksum

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SPMDemoFramework",
-            url: "https://github.com/reemabhalani/SPMDemoPackage/releases/download/1.0.5/SPMDemoFramework-1.0.5.xcframework.zip",
-            checksum: "0c0288d921d66f31afbec2f0196e898d9ad1d56ac81e8d6f3c5a99c98a49a4e3"
+            url: "https://github.com/reemabhalani/SPMDemoPackage/releases/download/1.0.6/SPMDemoFramework-1.0.6.xcframework.zip",
+            checksum: "1c0288d921d66f31afbec2f0196e898d9ad1d56ac81e8d6f3c5a99c98a49a4e3"
         )
     ]
 )
